c10d40695480f1b024e7c45317e8b3b24bbd368e
atkox <atkox210715@gmail.com>
2016-11-01 07:46:41 +0000

    add region and profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
   
-  def show # 追加
-   @user = User.find(params[:id])
-  end
+  before_action :set_user, only: [:show, :edit, :update]
   
   def new
     @user = User.new
+  end
+  
+  def show
+   ##privateで設定済み。
   end
   
   def create
@@ -19,14 +21,39 @@ class UsersController < ApplicationController
   end
   
   def edit
-    
+    if my_user
+        render "edit"
+      else
+        render "show"
+    end
   end
+  
+ def update
+    if my_user
+      if @user.update(user_params)
+        flash[:success] = "プロフィールを更新しました"
+        redirect_to @user
+      else
+        render "edit"
+      end
+    else
+      render "show"
+    end
+ end
   
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name,:email,:region,:profile,:password,:password_confirmation)
   end
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
+  def my_user
+    @user==current_user
+  end
+  
 end
